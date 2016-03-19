4269f0bdcc4a12ea41dd947288800efb5100424f
Alan95649 <alan.c.wilkening@gmail.com>
2016-03-19 14:21:42 -0400

    code book file

---
+++ CodeBook.md
@@ -0,0 +1,28 @@
+CodeBook.md
+
+CodeBook for Cousera "getting and cleaning data" end of class project.
+Chris W. 3/19/2016
+
+Raw data is described at the below link.
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+Raw data was obtained from the below link.
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Variables in the output file "tidyData.txt"
+SubjectNumber: Human participant identification number 1-30
+Activity: Activity performed by participant, (walking, laying, etc)
+Content
+	>> Variable names were appended to but not modified from the source data, see source
+		documentation for full explanation of variable names.
+	>> No unit or coordinate transformations were performed, all units remain in g 
+		as specified in the original data set.
+	>> "_aggMean" was appended to indicate that the channel represents the aggregate
+		mean of the source data channel.
+
+Data within the output file "tidyData.txt" represent the aggregated mean of 
+the mean and standard deviation from the source data files for each 
+subject for a given activity.
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,113 @@
+run_analysis <- function() {
+    # "Getting and Cleaning" data end of class project
+    # see README.md for details of data origin and usage
+    # 
+    # Chris W. 3/19/16
+    # 
+    
+    library(plyr)
+ 
+    
+    # --- Specify where data files are ---
+    rootFolder <- "E:/CourseRA/DataScienceSpecialization/Class3_GettingCleaningData/week_4/EndOfClassProject/UCI HAR Dataset/"
+    outputFile <- "E:/CourseRA/DataScienceSpecialization/Class3_GettingCleaningData/week_4/EndOfClassProject/tidyData.txt"
+    
+    ## Common files to both test and training folders
+    actLabelsFile   <- paste0(rootFolder, "activity_labels.txt") # names of activities; 1-6, WALKING-LAYING-ETC
+    colNamesFile    <- paste0(rootFolder, "features.txt")        # 562 col names of what is within data files
+    
+    # Training data sets
+    subTrainFile    <- paste0(rootFolder, "train/", "subject_train.txt") # participant number, 1 column sub set of 1-30
+    x_trainFile     <- paste0(rootFolder, "train/", "X_train.txt")      # acc data, 562 columns 
+    y_trainFile     <- paste0(rootFolder, "train/", "Y_train.txt")      # activity code data, 1 column containing 1-6
+    # Test data sets
+    subTestFile     <- paste0(rootFolder, "test/", "subject_test.txt")  # participant number, 1 column sub set of 1-30
+    x_testFile      <- paste0(rootFolder, "test/", "X_test.txt")        # acc data, 562 columns 
+    y_testFile      <- paste0(rootFolder, "test/", "Y_test.txt")        # activity code data, 1 column containint 1-6
+    
+    # --- Load raw data ---
+    # common set
+    actlabel_raw    <- read.table(actLabelsFile, header = FALSE)
+    colNames_raw    <- read.table(colNamesFile, header = FALSE, stringsAsFactors = FALSE)
+    # training set 
+    subTrain_raw    <- read.table(subTrainFile, header = FALSE)
+    x_train_raw     <- read.table(x_trainFile, header = FALSE)
+    y_train_raw     <- read.table(y_trainFile, header = FALSE)
+    # test set
+    subTest_raw     <- read.table(subTestFile, header = FALSE)
+    x_test_raw      <- read.table(x_testFile, header = FALSE)
+    y_test_raw      <- read.table(y_testFile, header = FALSE)
+    
+    ## --- "Part 1: Merges the TRAINING and the TEST sets to 
+    ##                  create one data set" ---
+    x_AccData       <- rbind(x_train_raw,  x_test_raw)  # acceleromater based data
+    y_ActLblData    <- rbind(y_train_raw,  y_test_raw)  # activity label
+    subjectData     <- rbind(subTrain_raw, subTest_raw) # subject number data
+    
+    # Check for NA's in the data
+    good <- complete.cases(x_AccData)
+    if ( NROW(good) != nrow(x_AccData)) {
+        warning("One (or more) incomplete cases were found in x_AccData.")
+    }
+    
+    
+    ## --- "Part 2: Extracts only the measurements on the mean 
+    ##                  and standard deviation for each measurement."
+    # Make vector of column indexes where 'features.txt' aka colNames_raw
+    #  contains '-mean', or '-std'
+    
+    iMeanStd <- union( grep("-mean", tolower(colNames_raw$V2)), 
+                       grep("-std", tolower(colNames_raw$V2)) )
+
+    # form new data frame of only mean and std terms
+    x_AccData_MeanStd   <- x_AccData[, iMeanStd]
+    colNames_MeanStd    <- colNames_raw$V2[iMeanStd]
+    
+    ## --- Part 3: Uses descriptive activity names to name the 
+    ##                  activities in the data set.
+    
+    # apply activity names to activity rows
+    desActNameVec <- join(y_ActLblData, actlabel_raw)
+
+    # apply activity names into numerical data set x_AccData_MeanStd
+    desActName_AccData_MeanStd <- cbind(desActNameVec$V2, x_AccData_MeanStd)
+    
+    ## --- Part 4: Appropriately label the data set with 
+    ##                  descriptive variable names.
+    
+    # replace genearic col names in data set with those from "features.txt"
+    names(desActName_AccData_MeanStd)[1] <- "Activity"
+    
+    names(desActName_AccData_MeanStd)[2:(length(colNames_MeanStd)+1)] <- colNames_MeanStd
+        
+    desActName_AccData_MeanStd <- cbind(subjectData, desActName_AccData_MeanStd)
+    names(desActName_AccData_MeanStd)[1] <- "SubjectNumber"
+    
+    ## --- From the data set in step 4, create a second, independent 
+    ##          tidy data set with the average of each variable for 
+    ##          each activity and each subject.
+    nc = NCOL(desActName_AccData_MeanStd)
+    dActName_aggMeanStd <- aggregate( desActName_AccData_MeanStd[,3:nc], 
+                            list(desActName_AccData_MeanStd$SubjectNumber, 
+                                 desActName_AccData_MeanStd$Activity),
+                            mean)
+    # Apply col names
+    names(dActName_aggMeanStd)[1:2] <- list("SubjectNumber", "Activity")
+    
+    # append "_aggMean" to col names indicating an aggregate mean
+    nc          <- NCOL(dActName_aggMeanStd)
+    badNames    <- names(dActName_aggMeanStd[3:nc])
+    betterNames <- paste0(badNames, "_aggMean")
+    names(dActName_aggMeanStd)[3:nc] <- betterNames
+
+    ## --- Export data, asked for in the introduction.  
+    
+    write.csv(dActName_aggMeanStd, outputFile,
+              append    = FALSE, 
+              row.names = FALSE)
+
+    print("end of run_analysis.R")
+    return(TRUE)
+}
+
+
